c627fcbf49d3f8ee792a11da10bea3d54f4cd65d
R0X0N <159176725+R0X0N@users.noreply.github.com>
2025-06-19 21:21:29 +0200

    db.php

--- db.php
+++ db.php
@@ -1,16 +1,15 @@
 <?php
-// Dane do połączenia z bazą danych
-$host = 'pogoda.database.windows.net';
-$db = 'BazaPogodowa';
-$user = 'Pawel';
-$pass = 'Admin123';
-
+$host = getenv('DB_HOST');   // pogoda.postgres.database.azure.com
+$db   = getenv('DB_NAME');   // WeatherDB
+$user = getenv('DB_USER');   // np. admin@pogoda
+$pass = getenv('DB_PASS');   // twoje hasło
 
 try {
-    $pdo = new PDO("sqlsrv:host=$host;dbname=$db", $user, $pass);
+    $dsn = "pgsql:host=$host;port=5432;dbname=$db;sslmode=require";
+    $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Połączenie udane!";
 } catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
+    echo "Błąd połączenia: " . $e->getMessage();
 }
 ?>
-
